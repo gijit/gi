4d812f66f4fbc916f7bb81f7329362e59ea6c0ce
Jason E. Aten <j.e.aten@gmail.com>
2018-01-12 14:13:09 -0600

    formating for readability

--- pkg/compiler/utf8_test.go
+++ pkg/compiler/utf8_test.go
@@ -9,7 +9,17 @@ import (
 
 func Test041RangeOverUtf8BytesInString(t *testing.T) {
 
-	cv.Convey(`From the https://blog.golang.org/strings blog example of a for-range loop over utf8 in strings: Given the string nihongo := "日本語" containing three multibyte utf8 characters and, the loop: for i, runeValue := range s { println(r); } should loop over whole utf8 runes and not individual bytes. So runeValue should take on each of the three full runes in turn.`, t, func() {
+	cv.Convey(`From the https://blog.golang.org/strings blog example`+
+		` of a for-range loop over utf8 in strings: `+
+		`Given the string nihongo := "日本語" containing `+
+		`three multibyte utf8 characters, `+
+		`then the loop:`+
+		``+
+		`      for i, runeValue := range s { println(r); }`+
+		``+
+		`should loop over whole utf8 runes and not `+
+		`over individual bytes. So runeValue should `+
+		`take on each of the three full runes in turn.`, t, func() {
 
 		// Resources:
 		//
@@ -21,25 +31,31 @@ func Test041RangeOverUtf8BytesInString(t *testing.T) {
 		// See the lua code in https://github.com/go-interpreter/gi/blob/master/pkg/utf8/utf8.lua
 		// See the Go code in https://github.com/go-interpreter/gi/blob/master/pkg/utf8/utf8.go
 
-		/* From Rob Pike's blog https://blog.golang.org/strings
+		// From Rob Pike's blog https://blog.golang.org/strings
+		//
+		// Besides the axiomatic detail that Go
+		// source code is UTF-8, there's really
+		// only one way that Go treats UTF-8 specially,
+		// and that is when using a for range loop on a string.
+		//
+		// We've seen what happens with a regular for loop.
+		// A for range loop, by contrast, decodes one
+		// UTF-8-encoded rune on each iteration. Each time
+		// around the loop, the index of the loop is the
+		// starting position of the current rune, measured
+		// in bytes, and the code point is its value.
+		//
+		// ...
+		//
+		// const nihongo = "日本語"
+		// for index, runeValue := range nihongo {
+		//    fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+		// }
 
-							Besides the axiomatic detail that Go source code is UTF-8, there's really only one way that Go treats UTF-8 specially, and that is when using a for range loop on a string.
-
-							We've seen what happens with a regular for loop. A for range loop, by contrast, decodes one UTF-8-encoded rune on each iteration. Each time around the loop, the index of the loop is the starting position of the current rune, measured in bytes, and the code point is its value.
-
-		            ...
-
-				    const nihongo = "日本語"
-				    for index, runeValue := range nihongo {
-				        fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
-				    }
-
-					    should print:
-
-					U+65E5 '日' starts at byte position 0
-					U+672C '本' starts at byte position 3
-					U+8A9E '語' starts at byte position 6
-		*/
+		// should print:
+		// U+65E5 '日' starts at byte position 0
+		// U+672C '本' starts at byte position 3
+		// U+8A9E '語' starts at byte position 6
 
 		// since we don't have fmt/imports online yet, we'll settle
 		// for just taking the string apart at the utf8 separation
