cae2ea91e76409e151f2b6ec18a238fe4b762951
Jason E. Aten <j.e.aten@gmail.com>
2018-01-12 14:04:15 -0600

    ft. 041 utf8_test for range loop over utf8 strings

---
+++ pkg/compiler/templates/flow10.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+// nested calls: can panic transfer
+// from lower in the stack to higher up?
+
+var global int = 8
+
+func deeper(a int) {
+	global += (a + 3)
+	panic("panic-in-deeper")
+}
+
+func intermed(a int) {
+	deeper(a)
+}
+
+func main() {
+	a := 0
+	b := 0
+
+	f := func() (ret0, ret1 int) {
+
+		defer func(a int) {
+			fmt.Printf("first defer running, a=%v, b=%v, ret0=%v, ret1=%v, global=%v\n",
+				a, b, ret0, ret1, global)
+			b = b + 3
+			ret0 = (ret0+1)*3 + global
+			fmt.Printf("debug, after ret0 = (ret0+1) * 3 + global: ret0=%v\n", ret0)
+			ret1 = ret1 + 1
+			recov := recover()
+			fmt.Printf("defer 1 recovered '%v'\n", recov)
+			if recov != nil {
+				fmt.Printf("recov was not nil, ret0=%v  and ret1=%v\n", ret0, ret1)
+				ret1 = ret1 + 9 + global
+				ret0 = ret0 + 19 + global
+			}
+			fmt.Printf("at end of 1st defer, ret0='%v', ret1='%v'\n", ret0, ret1)
+		}(a)
+
+		intermed(a)
+		return
+	}
+	f1, f2 := f()
+	fmt.Printf("f1 = %v, f2 = %v\n", f1, f2)
+	/*
+		go run flow10.go
+		first defer running, a=0, b=0, ret0=0, ret1=0, global=11
+		debug, after ret0 = (ret0+1) * 3 + global: ret0=14
+		defer 1 recovered 'panic-in-deeper'
+		recov was not nil, ret0=14  and ret1=1
+		at end of 1st defer, ret0='44', ret1='21'
+		f1 = 44, f2 = 21
+	*/
+}
---
+++ pkg/compiler/utf8_test.go
@@ -0,0 +1,76 @@
+package compiler
+
+import (
+	"testing"
+
+	cv "github.com/glycerine/goconvey/convey"
+	luajit "github.com/glycerine/golua/lua"
+)
+
+func Test041RangeOverUtf8BytesInString(t *testing.T) {
+
+	cv.Convey(`From the https://blog.golang.org/strings blog example of a for-range loop over utf8 in strings: Given the string nihongo := "日本語" containing three multibyte utf8 characters and, the loop: for i, runeValue := range s { println(r); } should loop over whole utf8 runes and not individual bytes. So runeValue should take on each of the three full runes in turn.`, t, func() {
+
+		// Resources:
+		//
+		// Rob Pike's blog: https://github.com/go-interpreter/gi/blob/master/pkg/utf8/utf8.go
+		//
+		// see https://github.com/go-interpreter/gi/issues/13 for discussion, and to note
+		//  progress and completion.
+		//
+		// See the lua code in https://github.com/go-interpreter/gi/blob/master/pkg/utf8/utf8.lua
+		// See the Go code in https://github.com/go-interpreter/gi/blob/master/pkg/utf8/utf8.go
+
+		/* From Rob Pike's blog https://blog.golang.org/strings
+
+							Besides the axiomatic detail that Go source code is UTF-8, there's really only one way that Go treats UTF-8 specially, and that is when using a for range loop on a string.
+
+							We've seen what happens with a regular for loop. A for range loop, by contrast, decodes one UTF-8-encoded rune on each iteration. Each time around the loop, the index of the loop is the starting position of the current rune, measured in bytes, and the code point is its value.
+
+		            ...
+
+				    const nihongo = "日本語"
+				    for index, runeValue := range nihongo {
+				        fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+				    }
+
+					    should print:
+
+					U+65E5 '日' starts at byte position 0
+					U+672C '本' starts at byte position 3
+					U+8A9E '語' starts at byte position 6
+		*/
+
+		// since we don't have fmt/imports online yet, we'll settle
+		// for just taking the string apart at the utf8 separation
+		// points. No printing f
+
+		code := `
+    runes := make([]rune, 3)
+    const nihongo = "日本語"  // translated, means "Japanese"
+    for i, runeValue := range nihongo {
+        runes[i] = runeValue
+    }
+`
+		inc := NewIncrState()
+		translation := inc.Tr([]byte(code))
+
+		cv.So(string(translation), cv.ShouldMatchModuloWhiteSpace,
+			`			(TODO: fill this in :) 
+`)
+
+		// and verify that it happens correctly
+		vm := luajit.NewState()
+		defer vm.Close()
+		vm.OpenLibs()
+		files, err := FetchPrelude(".")
+		panicOn(err)
+		LuaDoFiles(vm, files)
+
+		LuaRunAndReport(vm, string(translation))
+		LuaMustInt(vm, "runes[0]", '日')
+		LuaMustInt(vm, "runes[1]", '本')
+		LuaMustInt(vm, "runes[2]", '語')
+
+	})
+}
