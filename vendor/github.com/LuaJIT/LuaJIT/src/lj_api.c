302022733b398291b2a42dc1347e91ddb3866080
Jason E. Aten <j.e.aten@gmail.com>
2018-01-18 11:59:44 +0700

    better typechecks, 056 green

--- vendor/github.com/LuaJIT/LuaJIT/src/lj_api.c
+++ vendor/github.com/LuaJIT/LuaJIT/src/lj_api.c
@@ -352,8 +352,15 @@ LUA_API int64_t lua_cdata_to_int64(lua_State *L, int idx)
   if (!tviscdata(o)) {
     lj_err_argt(L, 1, LUA_TCDATA);
   }
-  
   GCcdata *cd = cdataV(o);
+
+  // neither of these work:
+  //CTypeID ctypeid = *(CTypeID *)cdataptr(cd);  
+  //CTypeID ctypeid = *(CTypeID *)cdataptr(o);
+  CTypeID ctypeid = cd->ctypeid;
+  if (ctypeid != 11) {
+    lj_err_argt(L, 1, LUA_TCDATA);
+  }
   return *(int64_t*)(cdataptr(cd));
 }
 
@@ -366,6 +373,14 @@ LUA_API uint64_t lua_cdata_to_uint64(lua_State *L, int idx)
   }
   
   GCcdata *cd = cdataV(o);
+
+  // neither of these work:
+  //CTypeID ctypeid = *(CTypeID *)cdataptr(cd);  
+  //CTypeID ctypeid = *(CTypeID *)cdataptr(o);
+  CTypeID ctypeid = cd->ctypeid;
+  if (ctypeid != 12) {
+    lj_err_argt(L, 1, LUA_TCDATA);
+  }
   return *(uint64_t*)(cdataptr(cd));
 }
 
