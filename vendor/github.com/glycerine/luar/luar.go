8bab61bc5c570c8cfb45f09518eff70756cf8d32
Jason E. Aten <j.e.aten@gmail.com>
2018-01-18 12:49:13 +0700

    quiet down

--- vendor/github.com/glycerine/luar/luar.go
+++ vendor/github.com/glycerine/luar/luar.go
@@ -835,17 +835,15 @@ func luaToGo(L *lua.State, idx int, v reflect.Value, visited map[uintptr]reflect
 		case 10: //  uint32
 		case 11: //  int64
 			val := L.CdataToInt64(idx)
-			pp("luar.go calling L.CdataToInt64, got val='%#v'", val)
+			//pp("luar.go calling L.CdataToInt64, got val='%#v'", val)
 			f := reflect.ValueOf(val)
 			v.Set(f.Convert(v.Type()))
-			// v.Set(f) // reflect.Set: value of type int64 is not assignable to type int
 			return nil
 		case 12: //  uint64
 			val := L.CdataToUint64(idx)
-			pp("luar.go calling L.CdataToUint64, got val='%#v'", val)
+			//pp("luar.go calling L.CdataToUint64, got val='%#v'", val)
 			f := reflect.ValueOf(val)
 			v.Set(f.Convert(v.Type()))
-			//v.Set(f)
 			return nil
 		case 13: //  float32
 		case 14: //  float64
