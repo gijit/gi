19238f4257d9f8798b76df21a7a977398f9e6472
Jason E. Aten <j.e.aten@gmail.com>
2018-01-20 13:06:07 +0700

    atg. hardcode lua version

--- cmd/gi/version.go
+++ cmd/gi/version.go
@@ -9,7 +9,7 @@ var BuildTimeStamp string
 var NearestGitTag string
 var GitBranch string
 var GoVersion string
-var LuajitVersion string
+var LuajitVersion string = "LuaJIT_2.1.0-beta3+jea-int64-additions"
 
 func Version() string {
 	return fmt.Sprintf("built: '%s'\nlast-git-commit-hash: '%s'\nnearest-git-tag: '%s'\ngit-branch: '%s'\ngo-version: '%s'\nluajit-version: '%s'", BuildTimeStamp, LastGitCommitHash, NearestGitTag, GitBranch, GoVersion, LuajitVersion)
