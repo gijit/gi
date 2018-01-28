540aea801741c07cb8c2308ee6e60e3cb0693ab9
Jason E. Aten <j.e.aten@gmail.com>
2018-01-28 22:06:32 +0700

    not finished struct test

--- pkg/muse/muse_test.go
+++ pkg/muse/muse_test.go
@@ -40,3 +40,18 @@ func Test001BasicTypeConversion(t *testing.T) {
 
 	})
 }
+
+func Test002StructTypeConversion(t *testing.T) {
+
+	cv.Convey(`muse.Pun() should convert a struct type.Types `+
+		`to a struct reflect.Types`, t, func() {
+
+		m := NewMuse()
+
+		b := types.Typ[types.Bool]
+		rt, err := m.Pun(b)
+		cv.So(err, cv.ShouldBeNil)
+		cv.So(rt.String(), cv.ShouldResemble, reflect.Bool.String())
+
+	})
+}
