5d7361bc255941985aed57977ecf942b8048ac78
Jason E. Aten <j.e.aten@gmail.com>
2018-01-31 21:21:14 +0700

    wip

--- pkg/compiler/face.lua
+++ pkg/compiler/face.lua
@@ -4,57 +4,104 @@ __gi_PrivateInterfaceProps = __gi_PrivateInterfaceProps or {}
 
 __gi_ifaceNil = __gi_ifaceNil or {[__gi_PrivateInterfaceProps]={name="nil"}}
 
+-- __gi_assertType is an interface type assertion.
+--
+--  either
+--    a, ok := b.(Face)  ## the two value form (returnTupe==2)
+--  or
+--    a := b.(Face)      ## the one value form (returnTuple==0; can panic)
+--  or
+--    _, ok := b.(Face)  ## (returnTuple==1; does not panic)
+--
+-- returnTuple in {0, 1, 2},
+--   0 returns just the interface-value, converted or nil/zero-value.
+--   1 returns just the ok (2nd-value in a conversion, a bool)
+--   2 returns both
+--
+--   if 0, then we may panic if the interface conversion fails.
+--
 function __gi_assertType(value, typ, returnTuple)
 
-  var isInterface = (typ.kind === __gi_kindInterface), ok, missingMethod = "";
-  if (value === __gi_ifaceNil) {
-    ok = false;
-  } else if (!isInterface) {
-    ok = value.constructor === typ;
-  } else {
-    var valueTypeString = value.constructor.string;
-    ok = typ.implementedBy[valueTypeString];
-    if (ok === undefined) {
-      ok = true;
-      var valueMethodSet = __gi_methodSet(value.constructor);
-      var interfaceMethods = typ.methods;
-      for (var i = 0; i < interfaceMethods.length; i++) {
-        var tm = interfaceMethods[i];
-        var found = false;
-        for (var j = 0; j < valueMethodSet.length; j++) {
-          var vm = valueMethodSet[j];
-          if (vm.name === tm.name && vm.pkg === tm.pkg && vm.typ === tm.typ) {
-            found = true;
-            break;
-          }
-        }
-        if (!found) {
-          ok = false;
-          typ.missingMethodFor[valueTypeString] = tm.name;
-          break;
-        }
-      }
-      typ.implementedBy[valueTypeString] = ok;
-    }
-    if (!ok) {
-      missingMethod = typ.missingMethodFor[valueTypeString];
-    }
-  }
+   print("__gi_assertType called, typ=", typ, " value=", value, " returnTuple=", returnTuple)
 
-  if (!ok) {
-    if (returnTuple) {
-      return [typ.zero(), false];
-    }
-    __gi_panic(new __gi_packages["runtime"].TypeAssertionError.ptr("", (value === __gi_ifaceNil ? "" : value.constructor.string), typ.string, missingMethod));
-  }
-
-  if (!isInterface) {
-    value = value.$val;
-  }
-  if (typ === $jsObjectPtr) {
-    value = value.object;
-  }
-  return returnTuple ? [value, true] : value;
-};
-
+   local isInterface = false
+   if typ == "kindInterface" then
+      isInterface = true
+   end
+   
+   local ok = false;
+   local missingMethod = "";
+   
+  if value == __gi_ifaceNil then
+     ok = false;
+     
+  elseif not isInterface then
+     ok = value.constructor == typ;
+     
+  else
+     local valueTypeString = value.constructor.string;
+     ok = typ.implementedBy[valueTypeString];
+     if ok == undefined then
+        
+        ok = true;
+        local valueMethodSet = __gi_methodSet(value.constructor);
+        local interfaceMethods = typ.methods;
+        local li = interfaceMethods.length
+        
+        for i = 0, li-1 do
+           
+           local tm = interfaceMethods[i];
+           local found = false;
+           local msl = valueMethodSet.length
+           
+           for j = 0,msl-1 do
+              local vm = valueMethodSet[j];
+              if vm.name == tm.name and vm.pkg == tm.pkg and vm.typ == tm.typ then
+                 found = true;
+                 break;
+              end
+           end
+           
+           if not found then
+              ok = false;
+              typ.missingMethodFor[valueTypeString] = tm.name;
+              break;
+           end
+        end
+        
+        typ.implementedBy[valueTypeString] = ok;
+        
+     end
+     if not ok then
+        missingMethod = typ.missingMethodFor[valueTypeString];
+     end
+  end
+  
+  if not ok then
+     
+     if returnTupe == 0 then
+        __gi_panic(new __gi_packages["runtime"].TypeAssertionError.ptr("", (value === __gi_ifaceNil ? "" : value.constructor.string), typ.string, missingMethod));
+        
+     elseif returnTuple == 1 then
+        return false
+     else
+        return zeroVal, false
+     end
+  end
+  
+  if not isInterface then
+     value = value.$val;
+  end
+  
+  if typ == $jsObjectPtr then
+     value = value.object;
+  end
+  
+  if returnTupe == 0 then
+     return value
+  elseif returnTuple == 1 then
+     return true
+  end
+  return value, true
+  
 end
