7fa7ebcdd228afb1694c46e9b30a0b68fae1e31f
Jason E. Aten <j.e.aten@gmail.com>
2018-01-31 14:31:38 +0700

    add ptr_test

---
+++ pkg/compiler/ptr_test.go
@@ -0,0 +1,39 @@
+package compiler
+
+import (
+	"fmt"
+	"testing"
+
+	cv "github.com/glycerine/goconvey/convey"
+)
+
+func Test098Pointers(t *testing.T) {
+
+	cv.Convey(`taking pointers and referencing through them`, t, func() {
+
+		code := `
+type S struct{
+  val int
+}
+var s S
+a := &s
+a.val = 10
+b := s.val
+`
+		vm, err := NewLuaVmWithPrelude(nil)
+		panicOn(err)
+		defer vm.Close()
+		inc := NewIncrState(vm, nil)
+
+		translation := inc.Tr([]byte(code))
+		fmt.Printf("\n translation='%s'\n", translation)
+
+		//		cv.So(string(translation), cv.ShouldMatchModuloWhiteSpace, ``)
+
+		// and verify that it happens correctly
+		LuaRunAndReport(vm, string(translation))
+
+		LuaMustInt64(vm, "b", 10)
+
+	})
+}
