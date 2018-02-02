e7765e33ec7ab9b74ce2f1547b3a506b9bd6ff30
Jason E. Aten <j.e.aten@gmail.com>
2018-02-02 17:00:27 +0700

    ft. 108 for type system corruption after syntax error

--- pkg/compiler/newtyp_test.go
+++ pkg/compiler/newtyp_test.go
@@ -2,6 +2,7 @@ package compiler
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	cv "github.com/glycerine/goconvey/convey"
@@ -88,12 +89,106 @@ g := F(3.4)
 		LuaMustFloat64(vm, "f", 2.5)
 		LuaMustFloat64(vm, "g", 3.4)
 
+		code2 := `	var h float64 = g;`
+		// expect:
+		// ./m.go:27:6: cannot use g (type F) as type float64 in assignment
+
+		gotPanic := false
+		var pval string
+		f := func() {
+			defer func() {
+				r := recover()
+				if r != nil {
+					gotPanic = true
+					pval = fmt.Sprintf("%v", r)
+				}
+			}()
+			translation2 := inc.Tr([]byte(code2)) // should panic
+			fmt.Printf("\n translation2='%s'\n", translation2)
+		}
+		f()
+		cv.So(gotPanic, cv.ShouldBeTrue)
+		cv.So(strings.Contains(pval, "cannot use g (variable of type F) as float64"),
+			cv.ShouldBeTrue)
+	})
+}
+
+func Test108SyntaxErrorDoesNotMessUpTypeSystem(t *testing.T) {
+
+	cv.Convey(`a syntax error was disabling subsequent type checks`, t, func() {
+
+		code := `
+type F float64
+var f F = 2.5
+`
+		vm, err := NewLuaVmWithPrelude(nil)
+		panicOn(err)
+		defer vm.Close()
+		inc := NewIncrState(vm, nil)
+
+		translation := inc.Tr([]byte(code))
+		fmt.Printf("\n translation='%s'\n", translation)
+
+		//cv.So(string(translation), cv.ShouldMatchModuloWhiteSpace, ``)
+
+		LuaRunAndReport(vm, string(translation))
+
+		LuaMustFloat64(vm, "f", 2.5)
+
+		gotPanic := false
+		var pval string
+
+		if true {
+			// this is a deliberate syntax error, but it
+			// is/was upsetting subsequent type checks:
+			code2 := `var float64 w`
+			// expect:
+			// ./m.go:27:6: cannot use g (type F) as type float64 in assignment
+
+			f := func() {
+				defer func() {
+					r := recover()
+					if r != nil {
+						gotPanic = true
+						pval = fmt.Sprintf("%v", r)
+					}
+				}()
+				translation2 := inc.Tr([]byte(code2)) // should panic
+				fmt.Printf("\n translation2='%s'\n", translation2)
+			}
+			f()
+			cv.So(gotPanic, cv.ShouldBeTrue)
+			cv.So(strings.Contains(pval, "undeclared name: w"),
+				cv.ShouldBeTrue)
+		}
+
+		code3 := `var a float64 = f`
+
+		gotPanic = false
+		pval = ""
+		f2 := func() {
+			defer func() {
+				r := recover()
+				if r != nil {
+					gotPanic = true
+					pval = fmt.Sprintf("%v", r)
+				}
+			}()
+			translation3 := inc.Tr([]byte(code3)) // should panic, but was not.
+			fmt.Printf("\n translation3='%s'\n", translation3)
+		}
+		f2()
+		fmt.Printf("pval = '%v'\n", pval)
+		cv.So(gotPanic, cv.ShouldBeTrue)
+		cv.So(strings.Contains(pval, "cannot use f (variable of type F) as float64 value in variable declaration"),
+			cv.ShouldBeTrue)
+
 	})
 }
 
 /*
 // unfinished
-func Test108TypesHavePackagePath(t *testing.T) {
+func Test110TypesHavePackagePath(t *testing.T) {
 
 	cv.Convey(`types at the repl have the "main" package (or whatever package we are working in) short name AND full path attached to their type, so we can distinguish types with the same name that come from different packages (even vendored) whose ultimate paths differ`, t, func() {
 
