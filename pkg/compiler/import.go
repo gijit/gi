b5e35c70f6b47185cae36b4844291bd51a83e31a
Jason E. Aten <j.e.aten@gmail.com>
2018-02-07 02:24:55 +0700

    atg. shadow math so import math now works

--- pkg/compiler/import.go
+++ pkg/compiler/import.go
@@ -11,6 +11,7 @@ import (
 	// shadow_ imports: available inside the REPL
 	"github.com/gijit/gi/pkg/compiler/shadow/fmt"
 	"github.com/gijit/gi/pkg/compiler/shadow/io"
+	"github.com/gijit/gi/pkg/compiler/shadow/math"
 	"github.com/gijit/gi/pkg/compiler/shadow/os"
 	"github.com/gijit/gi/pkg/compiler/shadow/regexp"
 )
@@ -67,6 +68,8 @@ func (ic *IncrState) GiImportFunc(path string) (*Archive, error) {
 		luar.Register(ic.vm, "os", shadow_os.Pkg)
 	case "io":
 		luar.Register(ic.vm, "io", shadow_io.Pkg)
+	case "math":
+		luar.Register(ic.vm, "math", shadow_math.Pkg)
 	default:
 		// need to run gen-gijit-shadow-import
 		return nil, fmt.Errorf("erro: package '%s' unknown, or not shadowed. To shadow it, run gen-gijit-shadow-import on the package, add a case and import above, and recompile gijit.", path)
