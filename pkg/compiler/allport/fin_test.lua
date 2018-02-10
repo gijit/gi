ab7499151999338ebdcf130460d964248c198a75
Jason E. Aten <j.e.aten@gmail.com>
2018-02-10 16:18:48 +0700

    progress on full.lua

--- pkg/compiler/allport/fin_test.lua
+++ pkg/compiler/allport/fin_test.lua
@@ -11,7 +11,7 @@ expectEq("a_b_c", __mapAndJoinStrings("_", {"a","b","c"}, function(x) return x e
 expectEq("a", __mapAndJoinStrings("_", {"a"}, function(x) return x end))
 expectEq("a", __mapAndJoinStrings("_", {[0]="a"}, function(x) return x end))
 expectEq("a_b", __mapAndJoinStrings("_", {[0]="a","b"}, function(x) return x end))
-expectEq("1_2", __mapAndJoinStrings("_", {[0]=0,1}, function(x)  return x+1 end))
 expectEq("1_2_3", __mapAndJoinStrings("_", {[0]=0,1,2}, function(x)  return x+1 end))
+expectEq("1_2", __mapAndJoinStrings("_", {[0]=0,1}, function(x)  return x+1 end))
 expectEq("1", __mapAndJoinStrings("_", {[0]=0}, function(x)  return x+1 end))
 expectEq("", __mapAndJoinStrings("_", {}, function(x)  return x+1 end))
