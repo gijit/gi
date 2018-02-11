8e5093a40c1901e1278f0e386336b11f61416197
Jason E. Aten <j.e.aten@gmail.com>
2018-02-11 03:22:52 -0600

    basic string type working in fin.lua, not yet returned it to half.lua

--- pkg/compiler/allport/fin.lua
+++ pkg/compiler/allport/fin.lua
@@ -1,4 +1,94 @@
 
+__kindBool = 1;
+__kindInt = 2;
+__kindInt8 = 3;
+__kindInt16 = 4;
+__kindInt32 = 5;
+__kindInt64 = 6;
+__kindUint = 7;
+__kindUint8 = 8;
+__kindUint16 = 9;
+__kindUint32 = 10;
+__kindUint64 = 11;
+__kindUintptr = 12;
+__kindFloat32 = 13;
+__kindFloat64 = 14;
+__kindComplex64 = 15;
+__kindComplex128 = 16;
+__kindArray = 17;
+__kindChan = 18;
+__kindFunc = 19;
+__kindInterface = 20;
+__kindMap = 21;
+__kindPtr = 22;
+__kindSlice = 23;
+__kindString = 24;
+__kindStruct = 25;
+__kindUnsafePointer = 26;
+
+-- st or showtable, a debug print helper.
+-- seen avoids infinite looping on self-recursive types.
+function __st(t, name, indent, quiet, methods_desc, seen)
+   if t == nil then
+      local s = "<nil>"
+      if not quiet then
+         print(s)
+      end
+      return s
+   end
+
+   seen = seen or {}
+   if seen[t] ~= nil then
+      return
+   end
+   seen[t] =true   
+   
+   if type(t) ~= "table" then
+      local s = tostring(t)
+      if not quiet then
+         if type(t) == "string" then
+            print('"'..s..'"')
+         else 
+            print(s)
+         end
+      end
+      return s
+   end   
+   
+   local k = 0
+   local name = name or ""
+   local namec = name
+   if name ~= "" then
+      namec = namec .. ": "
+   end
+   local indent = indent or 0
+   local pre = string.rep(" ", 4*indent)..namec
+   local s = pre .. "============================ "..tostring(t).."\n"
+   for i,v in pairs(t) do
+      k=k+1
+      local vals = ""
+      if methods_desc then
+         --print("methods_desc is true")
+         --vals = __st(v,"",indent+1,quiet,methods_desc, seen)
+      else 
+         vals = tostring(v)
+      end
+      s = s..pre.." "..tostring(k).. " key: '"..tostring(i).."' val: '"..vals.."'\n"
+   end
+   if k == 0 then
+      s = pre.."<empty table>"
+   end
+
+   local mt = getmetatable(t)
+   if mt ~= nil then
+      s = s .. "\n"..__st(mt, "mt.of."..name, indent+1, true, methods_desc, seen)
+   end
+   if not quiet then
+      print(s)
+   end
+   return s
+end
+
 -- apply fun to each element of the array arr,
 -- then concatenate them together with splice in
 -- between each one. It arr is empty then we
@@ -36,29 +126,37 @@ __keys = function(m)
    return r
 end
 
+__basicTypeMT = {
+   __tostring = function(self, ...)
+      return tostring(self.__val)
+   end
+}
+
 
 __tfunMT = {
    __name = "__tfunMT",
-   __call = function(the_mt, self, ...)
-      print("jea debug: __tfunMT.__call() invoked, self='",tostring(self),"', with tfun = ",self.tfun" and args=")
+   __call = function(self, ...)
+      --print("jea debug: __tfunMT.__call() invoked, self='"..tostring(self).."' with tfun = ".. tostring(self.tfun).. " and args=")
       
-      print("in __tfunMT, start __st on ...")
-      __st({...}, "__tfunMT.dots")
-      print("in __tfunMT,   end __st on ...")
+      --print("in __tfunMT, start __st on ...")
+      --__st({...}, "__tfunMT.dots")
+      --print("in __tfunMT,   end __st on ...")
 
-      print("in __tfunMT, start __st on self")
-      __st(self, "self")
-      print("in __tfunMT,   end __st on self")
+      --print("in __tfunMT, start __st on self")
+      --__st(self, "self")
+      --print("in __tfunMT,   end __st on self")
 
+      local newInstance = {}
+      setmetatable(newInstance, __basicTypeMT)
       if self ~= nil and self.tfun ~= nil then
-         print("calling tfun! -- let constructors set metatables if they wish to.")
-         self.tfun({}, ...)
+         --print("calling tfun! -- let constructors set metatables if they wish to.")
+         self.tfun(newInstance, ...)
       else
          if self ~= nil then
-            print("self.tfun was nil")
+            --print("self.tfun was nil")
          end
       end
-      return self
+      return newInstance
    end
 }
 
@@ -86,7 +184,10 @@ __newType = function(size, kind, str, named, pkg, exported, constructor)
 
   elseif kind == __kindString then
      
-    typ.tfun = function(this, v) this.__val = v; end;
+     typ.tfun = function(this, v)
+        --print("strings' tfun called! with v='"..tostring(v).."' and this:")
+        --__st(this)
+        this.__val = v; end;
     typ.wrapped = true;
     typ.keyFor = function(x) return "_" .. x; end;
 
@@ -134,3 +235,9 @@ __newType = function(size, kind, str, named, pkg, exported, constructor)
   return typ;
   
 end
+
+__Bool          = __newType( 1, __kindBool,          "bool",           true, "", false, nil);
+__Int           = __newType( 4, __kindInt,           "int",            true, "", false, nil);
+__Float64       = __newType( 8, __kindFloat64,       "float64",        true, "", false, nil);
+__String        = __newType( 8, __kindString,        "string",         true, "", false, nil);
+
