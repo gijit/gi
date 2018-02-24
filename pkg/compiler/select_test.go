db18fc220e1c8fdfc3b6184bcb5e25d6bd5b4d39
Jason E. Aten <j.e.aten@gmail.com>
2018-02-24 00:01:50 -0600

    ft. 604 select_test for select

--- pkg/compiler/select_test.go
+++ pkg/compiler/select_test.go
@@ -199,3 +199,68 @@ func Test603RecvOnUnbufferedChannel(t *testing.T) {
 		cv.So(true, cv.ShouldBeTrue)
 	})
 }
+
+func Test604Select(t *testing.T) {
+
+	cv.Convey(`in Lua, select on two channels`, t, func() {
+
+		vm, err := NewLuaVmWithPrelude(nil)
+		panicOn(err)
+		defer vm.Close()
+
+		chInt := make(chan int)
+		chStr := make(chan string)
+
+		luar.Register(vm, "", luar.Map{
+			"chInt": chInt,
+			"chStr": chStr,
+		})
+
+		// first run instantiates the main package so we can add 'ch' to it.
+		code := `b := 3`
+		inc := NewIncrState(vm, nil)
+		translation, err := inc.Tr([]byte(code))
+		panicOn(err)
+		pp("translation='%s'", string(translation))
+		LuaRunAndReport(vm, string(translation))
+		LuaMustInt64(vm, "b", 3)
+
+		// allow channels to type check
+		pkg := inc.pkgMap["main"].Arch.Pkg
+		scope := pkg.Scope()
+
+		nt := types.Typ[types.Int]
+		chIntVar := types.NewVar(token.NoPos, pkg, "chInt", types.NewChan(types.SendRecv, nt))
+		scope.Insert(chIntVar)
+
+		stringType := types.Typ[types.String]
+		chStrVar := types.NewVar(token.NoPos, pkg, "chStr", types.NewChan(types.SendRecv, stringType))
+		scope.Insert(chStrVar)
+
+		code = `
+a := 0
+b := ""
+for i := 0; i < 2; i++ {
+  select {
+    case a = <- chInt:
+    case b = <- chStr:
+  }
+}
+`
+		translation, err = inc.Tr([]byte(code))
+		panicOn(err)
+		pp("translation='%s'", string(translation))
+
+		go func() {
+			chInt <- 43
+		}()
+		go func() {
+			chStr <- "hello select"
+		}()
+		LuaRunAndReport(vm, string(translation))
+
+		LuaMustInt64(vm, "a", 43)
+		LuaMustString(vm, "b", "hello select")
+		cv.So(true, cv.ShouldBeTrue)
+	})
+}
