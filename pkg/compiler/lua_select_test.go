eb2a5ced8a908ea8771a9600a29811a2af7cfc23
Jason E. Aten <j.e.aten@gmail.com>
2018-02-25 13:29:52 -0600

    ft. 800 for all-lua channel send and receive

---
+++ pkg/compiler/lua_select_test.go
@@ -0,0 +1,40 @@
+package compiler
+
+// lua-only verion of select_test
+
+// For the moment, we'll comment this out while
+// we get the fully lua system going.
+
+import (
+	"testing"
+
+	cv "github.com/glycerine/goconvey/convey"
+	"github.com/glycerine/luar"
+)
+
+func Test800LuaOnlyRecvOnChannel(t *testing.T) {
+
+	cv.Convey(`in all-Lua, receive an integer on a buffered channel, previously sent by Lua`, t, func() {
+
+		vm, err := NewLuaVmWithPrelude(nil)
+		panicOn(err)
+		defer vm.Close()
+
+		ch := make(chan int, 1)
+		ch <- 57
+
+		luar.Register(vm, "", luar.Map{
+			"ch": ch,
+		})
+
+		code := `ch:=make(chan int, 1); ch <- 57; a:= <- ch`
+		inc := NewIncrState(vm, nil)
+		translation, err := inc.Tr([]byte(code))
+		panicOn(err)
+		pp("translation='%s'", string(translation))
+		LuaRunAndReport(vm, string(translation))
+		LuaMustInt64(vm, "a", 57)
+
+		cv.So(true, cv.ShouldBeTrue)
+	})
+}
