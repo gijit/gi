dbfb7a7bdb133ff5a916f573bd0c1c60edb5ebbd
Jason E. Aten <j.e.aten@gmail.com>
2018-03-02 14:12:00 -0500

    atg. add cmd/gen_static_prelude to statically embed the prelude files

---
+++ cmd/gen_static_prelude/prep.go
@@ -0,0 +1,32 @@
+// prep the prelude for static inclusion with the
+// `gi` binary.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/shurcooL/vfsgen"
+)
+
+func main() {
+
+	gopath := os.Getenv("GOPATH")
+	compiler := gopath + "/src/github.com/gijit/gi/pkg/compiler"
+	prelude := compiler + "/prelude"
+	gentarget := compiler + "/prelude_genstatic.go"
+	var fs http.FileSystem = http.Dir(prelude)
+
+	err := vfsgen.Generate(fs, vfsgen.Options{
+		Filename:    gentarget,
+		PackageName: "compiler",
+		//BuildTags: "!dev",
+		VariableName: "preludeFiles",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("prep '%s' ->\n   '%s'\n", prelude, gentarget)
+}
