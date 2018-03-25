42f1a76aaf6d88333ad4e843c4dd5ab76b866a78
Jason E. Aten <j.e.aten@gmail.com>
2018-03-24 20:15:44 -0500

    make install creates gijit_luajit so we can keep it distinct from native luajit

--- posix.sh
+++ posix.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "posix.sh: Doing one time build of libluajit.a"
 cd vendor/github.com/LuaJIT/LuaJIT && make clean && cd src && XCFLAGS=-DLUAJIT_ENABLE_GC64 make libluajit.a
+make gijit_luajit && cp -p gijit_luajit ${GOPATH}/bin/
+
--- windows.bat
+++ windows.bat
@@ -1,4 +1,5 @@
 @echo "windows.bat: Doing one time build of libluajit.a"
 cd vendor\github.com\LuaJIT\LuaJIT\src
 make libluajit.a
+make gijit_luajit
 cd ..\..\..\..\..
