4f5294e05c6baf9a1919b5a5703a912954873183
Jason E. Aten <j.e.aten@gmail.com>
2018-03-27 10:22:36 -0500

    start of depend_test.go

---
+++ pkg/compiler/depend_test.go
@@ -0,0 +1,16 @@
+package compiler
+
+import (
+	//"fmt"
+	"testing"
+
+	cv "github.com/glycerine/goconvey/convey"
+)
+
+func Test1200DepthFirstSearchOfTypeDependencies(t *testing.T) {
+
+	cv.Convey("dfs on type tree should work", t, func() {
+
+		cv.So(true, cv.ShouldBeTrue)
+	})
+}
