6f74258dad1d21ee27a08147f87d761bab9d08ab
Jason E. Aten <j.e.aten@gmail.com>
2018-03-27 15:18:05 -0500

    ft. 901, 087, 1002, 1004, 016b, but progress we think

--- pkg/compiler/fullpkg.go
+++ pkg/compiler/fullpkg.go
@@ -519,7 +519,13 @@ func FullPackageCompile(importPath string, files []*ast.File, fileSet *token.Fil
 		typeDecls = append(typeDecls, &d)
 	}
 
-	var allDecls []*Decl
+	var linearTypeGraph bytes.Buffer
+	c.p.typeDepend.genCode(&linearTypeGraph)
+	lin := &Decl{
+		TypeInitCode: linearTypeGraph.Bytes(),
+	}
+
+	allDecls := []*Decl{lin}
 	for _, d := range append(append(append(importDecls, typeDecls...), varDecls...), funcDecls...) {
 		d.DeclCode = removeWhitespace(d.DeclCode, minify)
 		d.MethodListCode = removeWhitespace(d.MethodListCode, minify)
