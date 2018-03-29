26c18f08261d2df3dd318d77a4090d955aed28f8
Jason E. Aten <j.e.aten@gmail.com>
2018-03-29 00:30:35 -0500

    latest

---
+++ pkg/compiler/roar/vprint.go
@@ -0,0 +1,25 @@
+package roar
+
+import (
+	"fmt"
+)
+
+type Stream struct {
+	Src string
+}
+
+var Verbose bool = false
+
+var A, B = true, false
+
+func P(format string, a ...interface{}) {
+	fmt.Printf("package spkg_tst2 var Verbose is %v\n", Verbose)
+	if Verbose {
+		fmt.Printf(format, a...)
+	}
+}
+
+func ToString(format string, a ...interface{}) string {
+	s := fmt.Sprintf("Verbose=%v", Verbose)
+	return s + fmt.Sprintf(format, a...)
+}
